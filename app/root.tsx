7d1867b5139f4b5d66e813e2d95e96cb0f49dcc6
疒奀 <46226846+mlegls@users.noreply.github.com>
2022-10-29 20:52:42 -0700

    change title

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "mlegls Projects",
   viewport: "width=device-width,initial-scale=1",
 });
 
